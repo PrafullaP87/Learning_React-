6bf6a77badd9e58ab3bf61d87f53b3e55fd20812
PrafullaP87 <prafullappatil87@gmail.com>
2024-02-25 12:37:49 +0530

    First commit

---
+++ my-app/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import  ReactDOM  from "react";
+
+ReactDOM.render(<div><h1>I am learning js</h1>
+<h1>Day 1</h1><h1>day 2</h1><h1>day 3</h1></div>
+,
+document.getElementById('root'));
